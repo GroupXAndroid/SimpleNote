388d74949144c7ed7bc8df180e251b5f899016a2
khanhhuy1110 <huynkhe153585@fpt.edu.vn>
2022-10-21 16:54:04 +0700

    Change note dao location

--- app/src/main/java/com/groupx/simplenote/activity/NoteDetail.java
+++ app/src/main/java/com/groupx/simplenote/activity/NoteDetail.java
@@ -3,13 +3,40 @@ import com.groupx.simplenote.R;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class NoteDetail extends AppCompatActivity {
+
+    private ImageView imageNoteDetailBack, imageNoteDetailSave;
+    private EditText editTextNoteSubtitle, editTextNoteTitle, editTextNoteContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_detail);
+
+        imageNoteDetailBack = findViewById(R.id.imageNoteDetailBack);
+        imageNoteDetailSave = findViewById(R.id.imageNoteDetailSave);
+
+        imageNoteDetailBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
+
+    private void saveNote(){
+        editTextNoteSubtitle = findViewById(R.id.editTextNoteTitle);
+        editTextNoteSubtitle = findViewById(R.id.editTextNoteSubtitle);
+        editTextNoteContent = findViewById(R.id.editTextNoteContent);
+
+    }
+
+
 }
