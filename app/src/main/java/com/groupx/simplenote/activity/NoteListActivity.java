fc83fc17cbfc419219c2c78660d6b4faa4f7a355
khanhhuy1110 <huynkhe153585@fpt.edu.vn>
2022-11-08 13:54:57 +0700

    Fix old NoteList

--- app/src/main/java/com/groupx/simplenote/activity/NoteListActivity.java
+++ app/src/main/java/com/groupx/simplenote/activity/NoteListActivity.java
@@ -1,6 +1,7 @@
 package com.groupx.simplenote.activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -12,6 +13,7 @@ import com.groupx.simplenote.R;
 import com.groupx.simplenote.adapter.NoteAdapter;
 import com.groupx.simplenote.common.Const;
 import com.groupx.simplenote.database.NoteDatabase;
+import com.groupx.simplenote.entity.Account;
 import com.groupx.simplenote.entity.Note;
 import com.groupx.simplenote.listener.NoteListener;
 
@@ -21,10 +23,11 @@ import java.util.List;
 public class NoteListActivity extends AppCompatActivity {
     private final int REQUEST_CODE_UPDATE_NOTE = 2;
 
-    private RecyclerView notesRecyclerView;
-    private NoteAdapter noteAdapter;
+    private RecyclerView rcvNoteList;
+    private NoteAdapter adapter;
+    private Account currentUser = new Account();
 
-    private List<Note> noteList;
+    private List<Note> noteList = new ArrayList<>();
 
     private int noteClickedPosition = -1;
 
@@ -32,49 +35,38 @@ public class NoteListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_list);
+        SharedPreferences sharedPreferences = getSharedPreferences(LoginActivity.ACCOUNT_ID, 0);
+        int accId = sharedPreferences.getInt("accountId", 0);
+        currentUser.setId(accId);
 
-        notesRecyclerView = findViewById(R.id.recyclerviewNote);
-        notesRecyclerView.setLayoutManager(
-                new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
-        );
+        rcvNoteList = findViewById(R.id.recyclerviewNote);
+        rcvNoteList.setLayoutManager(new StaggeredGridLayoutManager(2,  StaggeredGridLayoutManager.VERTICAL));
+        adapter = new NoteAdapter(noteList, this);
+        rcvNoteList.setAdapter(adapter);
 
-        noteList = new ArrayList<>();
-//        noteAdapter = new NoteAdapter(noteList, this);
-//        notesRecyclerView.setAdapter(noteAdapter);
-
-//        getNotes();
+        getNotes(Const.NoteRequestCode.REQUEST_CODE_SHOW);
     }
 
-//    @Override
-//    public void onNoteClicked(Note note, int position) {
-//        noteClickedPosition = position;
-//        Intent intent = new Intent(getApplicationContext(), CreateNoteActivity.class);
-//        intent.putExtra("note", note);
-//        intent.putExtra("mode", Const.NoteDetailActivityMode.EDIT);
-//        startActivityForResult(intent, REQUEST_CODE_UPDATE_NOTE);
-//    }
+    private int noteClikedPosition;
 
-//    private void getNotes() {
-//        String txtSearch = getIntent().getStringExtra("txtSearch");
-//        List<Note> notes = new ArrayList<>();
-//        if(txtSearch == null || txtSearch == "" || txtSearch.isEmpty()){
-//            notes = NoteDatabase.getSNoteDatabase(getApplicationContext())
-//                    .noteDao().getAllMyNote();
-//        }else{
-//            notes = NoteDatabase.getSNoteDatabase(getApplicationContext())
-//                    .noteDao().searchNote(txtSearch);
-//            if(notes == null || notes.size() == 0){
-//                Toast.makeText(this, "NO NOTES FOUND", Toast.LENGTH_SHORT).show();
-//            }
-//        }
-//        if (noteList.size() == 0) {
-//            noteList.addAll(notes);
-//            noteAdapter.notifyDataSetChanged();
-//        } else {
-//            noteList.add(0, notes.get(0));
-//            noteAdapter.notifyItemInserted(0);
-//
-//        }
-//        notesRecyclerView.smoothScrollToPosition(0);
-//    }
+    private void getNotes(int requestCode){
+        List<Note> notes = NoteDatabase.getSNoteDatabase(getApplicationContext())
+                .noteDao().getAllMyNote(currentUser.getId(),new int[] {Const.NoteStatus.NORMAL, Const.NoteStatus.FAVORITE});
+        if(requestCode == Const.NoteRequestCode.REQUEST_CODE_SHOW){
+            noteList.addAll(notes);
+            adapter.notifyDataSetChanged();
+        }else if(requestCode == Const.NoteRequestCode.REQUEST_CODE_CREATE){
+            noteList.add(0, notes.get(0));
+            adapter.notifyItemInserted(0);
+            rcvNoteList.smoothScrollToPosition(0);
+        }else if(requestCode == Const.NoteRequestCode.REQUEST_CODE_UPDATE){
+            noteList.remove(noteClikedPosition);
+            noteList.add(noteClikedPosition, notes.get(noteClikedPosition));
+            adapter.notifyItemChanged(noteClikedPosition);
+        }
+        else if(requestCode == Const.NoteRequestCode.REQUEST_CODE_DELETE){
+            noteList.remove(noteClikedPosition);
+            adapter.notifyItemRemoved(noteClikedPosition);
+        }
+    }
 }
